a2a9172cbb12fc40246ef7d3a97bfa9152e922ae
Zurceel <gabrielcruzsil@gmail.com>
2022-08-17 14:02:43 -0300

    Adicionar arquivos de projeto.

---
+++ Aula10/FormaPagamento.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula10
+{
+	public class FormaPagamento
+	{
+		public virtual void EfetuarPagamento()
+		{
+			Console.WriteLine("Pagamento efetuado");
+		}
+	}
+
+	public class Boleto : FormaPagamento
+	{
+		public override void EfetuarPagamento()
+		{
+			Console.WriteLine("Pagamento efetuado por boleto");
+		}
+	}
+
+	public class Pix : FormaPagamento
+	{
+		public override void EfetuarPagamento()
+		{
+			Console.WriteLine("Pagamento efetuado por pix");
+		}
+	}
+
+	public class Cartao : FormaPagamento
+	{
+
+		public Cartao()
+		{
+			EfetuarPagamento();
+		}
+
+		public override void EfetuarPagamento()
+		{
+			Console.WriteLine("Pagamento efetuado por cartão de crédito");
+		}
+	}
+}
---
+++ Aula10/Program.cs
@@ -0,0 +1,29 @@
+﻿using Aula10;
+
+Console.WriteLine("Digite três produtos a serem comprados:");
+
+List<string> produtos = new List<string>();
+
+for (int i = 0; i < 3; i++)
+{
+	produtos.Add(Console.ReadLine());
+}
+
+Console.WriteLine("Qual forma de pagamento você deseja? (Pix, Boleto, Cartão)");
+
+string formapagamentodesejada = Console.ReadLine();
+
+if(formapagamentodesejada == FormasPagamentoEnum.Boleto.ToString())
+{
+	Boleto boleto = new Boleto();
+	boleto.EfetuarPagamento();
+}
+else if(formapagamentodesejada == FormasPagamentoEnum.Pix.ToString())
+{
+	Pix pix = new Pix();
+	pix.EfetuarPagamento();
+}
+else if (formapagamentodesejada == FormasPagamentoEnum.Cartao.ToString())
+{
+	new Cartao();
+}
